4ef20a75a841c729dbb73d29a230336b6f0d3829
bubentsov-s <s.bubentsov@mail.ru>
2022-11-05 00:58:55 +0300

    добавлена папка Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,19 @@
+﻿// 14. Написать программу, которая
+// 1: принимает на вход число
+// 2: проверяет, кратно ли оно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int MultipleNumber(int number)
+{
+    return number % (7 * 23);
+}
+
+Console.Write("Введите целое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int rest = MultipleNumber(a);
+Console.WriteLine(rest == 0 ? "Кратно" : $"Не кратно, остаток {rest}");

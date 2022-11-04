0308122e4c904a9c225bb84419f0469e22719ecc
bubentsov-s <s.bubentsov@mail.ru>
2022-11-04 17:52:45 +0300

    добавил Task_02Homework

---
+++ Task_02(homework)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу,
+// которая на вход принимает 
+// два числа и выдаёт, какое число
+// большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.Write("Введите число №1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число №2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b) Console.Write("Числа равны");
+else if (a > b) Console.WriteLine($" Число {a} > Числа {b} ");
+else if (a < b) Console.WriteLine($" Число {b} > Числа {a} ");

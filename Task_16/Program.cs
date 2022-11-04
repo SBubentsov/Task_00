11d21c7efd4fdfe1f7fbfe41d82f60c011ebe433
bubentsov-s <s.bubentsov@mail.ru>
2022-11-05 01:47:31 +0300

    добавлена папка Task_16

---
+++ Task_16/Program.cs
@@ -0,0 +1,25 @@
+﻿// 16. Написать программу, которая
+// 1: принимает на вход два числа
+// 2: проверят, явлется ли одно число квадратом второго
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+bool Square(int num1, int num2)
+{
+    return num2*num2 == num1 || num1*num1 == num2;
+}
+
+Console.Write("Введите число №1: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число №2: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool square = Square(a, b);
+
+Console.Write(square ? "да" : "нет");

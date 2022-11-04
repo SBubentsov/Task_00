fd3a72b93362479efab1297ccd35cefdeea34059
bubentsov-s <s.bubentsov@mail.ru>
2022-11-04 23:45:09 +0300

    добавили Task_12

--- Task_11/Program.cs
+++ Task_11/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу котора
+// 1: выводит случайное трехзначное число
+// 2: удаляет вторую цифру этого числа
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int MaxDigits(int number)
+{
+    int firstDigit = num / 100;
+    int therdDigit = num % 10;
+    return firstDigit > therdDigit ? firstDigit : secondDigit;
+}
+
+
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число -> {num}");
+

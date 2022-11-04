0b945f6b37c682d9848c87b33e88d2a0e20d622e
bubentsov-s <s.bubentsov@mail.ru>
2022-11-04 17:29:29 +0300

    добавили Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -12,12 +12,12 @@ int a = Convert.ToInt32(Console.ReadLine());
 if ( a > 0 && a < 8 )
     {
         if (a == 1) Console.WriteLine("Понедельник");
-        if (a == 2) Console.WriteLine("Вторник");
-        if (a == 3) Console.WriteLine("Среда");
-        if (a == 4) Console.WriteLine("Четверг");
-        if (a == 5) Console.WriteLine("Пятница");
-        if (a == 6) Console.WriteLine("Суббота");
-        if (a == 7) Console.WriteLine("Воскресение");
+        else if (a == 2) Console.WriteLine("Вторник");
+        else if (a == 3) Console.WriteLine("Среда");
+        else if (a == 4) Console.WriteLine("Четверг");
+        else if (a == 5) Console.WriteLine("Пятница");
+        else if (a == 6) Console.WriteLine("Суббота");
+        else Console.WriteLine("Воскресение");
     }
 
     else

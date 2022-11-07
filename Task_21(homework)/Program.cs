041dc746bbd6ed9c946fb781066ee20f19d06404
bubentsov-s <s.bubentsov@mail.ru>
2022-11-07 19:22:07 +0300

    добавлена Task_21(homework)

--- Task_21(homework)/Program.cs
+++ Task_21(homework)/Program.cs
@@ -1,25 +1,28 @@
-﻿// 20. Напишите программу, которая
-// 1: принимает координаты двух точек
-// 2: находит расстояние между ними в 2D пространстве.
+﻿// 21. Напишите программу, которая 
+// 1: принимает на вход координаты двух точек
+// 2: находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.WriteLine("Введите координаты точки A.");
 Console.Write("X: ");
 int xa = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int za = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты точки B.");
 Console.Write("X: ");
 int xb = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 int yb = Convert.ToInt32(Console.ReadLine());
-
-// double l = Math.Sqrt((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb));
-// double lenght = Math.Round(l, 2, MidpointRounding.ToZero);
-// Console.WriteLine($"Длинна отрезка по заданным точкам: {lenght}");
+Console.Write("Z: ");
+int zb = Convert.ToInt32(Console.ReadLine());
 
 
-double l = Math.Sqrt( Square(xa, xb) + Square(ya, yb));
+
+double l = Math.Sqrt( Square(xa, xb) + Square(ya, yb) + Square(za, zb));
 double length = Math.Round(l, 2, MidpointRounding.ToZero);
 Console.WriteLine($"Длинна отрезка по заданным точкам: {length}");
 

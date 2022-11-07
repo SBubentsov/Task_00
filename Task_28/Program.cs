2a2598a1d02c43f467cf78917182e73b50d7367d
bubentsov-s <s.bubentsov@mail.ru>
2022-11-07 21:57:24 +0300

    добавлены Task_28 и Task_30

---
+++ Task_28/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+
+Console.Write("Введите число больше 1: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int Multiplication(int n)
+{
+    int mult = 1;
+   
+    for (int i = 1; i <= n; i++)
+    {
+        mult *= i;
+    }
+ return mult;
+}
+
+if (a > 0)
+{
+    int multiplication = Multiplication(a);
+    Console.WriteLine($"Произведение чисел от 1 до {a}: {multiplication}");
+}
+else Console.WriteLine("Введено некорректное число, требуется целое положительное число.");
---
+++ Task_30/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 30: Напишите программу, которая
+// 1: выводит массив из N элементов, заполненный
+// 2: нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+   
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+ return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} | ");
+    }
+}
+
+if (a > 0)
+{
+ int[] arr= CreateArray(a);
+ PrintArray(arr);
+}
+else Console.WriteLine("Введено некорректное число, требуется целое положительное число.");

62e5ed1b01df3862d5a2505ce88a7aaea7a34b53
bubentsov-s <s.bubentsov@mail.ru>
2022-11-07 19:42:32 +0300

    добавлена Task_22

---
+++ Task_22/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 22: Напишите программу, которая
+// 1: на вход принимает число (N), 
+// 2: выдает таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+void Table(int n)
+{
+    if (n > 0)
+    {
+        int count = 1;
+        Console.WriteLine("Все квадраты чисел от 1 до указанного числа:");
+        while (count <= n)
+        {
+
+            Console.WriteLine($"{count} | {count * count}");
+            count += 1;
+        }
+    }
+    else Console.WriteLine("Недопустимое число");
+}
+
+
+Console.Write("Введите число больше 1: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+// if ( a < 2 )
+
+//     Console.WriteLine("Недопустимое число");
+// else
+// {
+//     int count = 2;
+//     Console.Write("Все квадраты чисел от 1 до указанного числа: 1 ");
+//     while (count <= a)
+//     {
+
+//         Console.Write($", {count*count}");
+//         count += 1;
+//     }
+// }    
+
+Table(a);

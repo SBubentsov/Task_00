71eb3a9a30b411a88b97032806fc313957849e32
bubentsov-s <s.bubentsov@mail.ru>
2022-11-10 21:22:16 +0300

    Добавлены Task_32 и Task_33

---
+++ Task_32/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 32: Напишите программу 
+// 1: замены элементов массива:
+// 2: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]} | ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine(" ]");
+}
+
+void MirrorArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+
+
+MirrorArray(arr);
+Console.WriteLine("Замена элементов массива на соответствующие с противоположным знаком.");
+PrintArray(arr);

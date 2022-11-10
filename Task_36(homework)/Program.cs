ad7ed4d3990ff4eb2f3933b56d2bcaff00ab1a27
bubentsov-s <s.bubentsov@mail.ru>
2022-11-10 23:06:36 +0300

    Добавлена Task_36(homework)

---
+++ Task_36(homework)/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]} | ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine(" ]");
+}
+
+int SummOddNumberArray(int[] array)
+{
+    int summ = 0;
+    for (int i = 1; i < array.Length; i+= 2) summ += array[i];
+    
+    return summ;
+}
+
+
+Console.WriteLine("Введите размер массива");
+int leng = Convert.ToInt32(Console.ReadLine());
+int minimum = -10;
+int maximum = 11;
+
+int[] arr = CreateArrayRndInt(leng, minimum, maximum);
+PrintArray(arr);
+int result = SummOddNumberArray(arr);
+
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях -> {result}");
